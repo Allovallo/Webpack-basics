e11fd09c759b6100bf205d8c25bbf6115edb7ac9
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-06-11 17:03:03 +0300

    update JS files
    update JS files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -12,6 +13,7 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: "styles.css",
       // chunkFilename: "[id].css",
